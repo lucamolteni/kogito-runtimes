5e353bf607da9bf394585945a036c862c0ee15c7
Luca Molteni <volothamp@gmail.com>
2019-11-13 17:45:50 +0100

    Updated mvel-compiler

--- drools/drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/context/MvelCompilerContext.java
+++ drools/drools-model/drools-mvel-compiler/src/main/java/org/drools/mvelcompiler/context/MvelCompilerContext.java
@@ -1,6 +1,5 @@
 package org.drools.mvelcompiler.context;
 
-import java.util.Collection;
 import java.util.Optional;
 
 import org.drools.core.addon.TypeResolver;
@@ -8,7 +7,7 @@ import org.drools.mvelcompiler.MvelCompilerException;
 
 public class MvelCompilerContext {
 
-    private Declarations declarations = new Declarations();
+    private ContextDeclarations contextDeclarations = new ContextDeclarations();
     private final TypeResolver typeResolver;
 
     public MvelCompilerContext(TypeResolver typeResolver) {
@@ -16,25 +15,25 @@ public class MvelCompilerContext {
     }
 
     public MvelCompilerContext addDeclaration(String name, Class<?> clazz) {
-        declarations.addDeclarations(new Declaration(name, clazz));
+        contextDeclarations.addDeclarations(new Declaration(name, clazz));
         return this;
     }
 
     public MvelCompilerContext addCreatedDeclaration(String name, Class<?> clazz) {
-        declarations.addDeclarations(new Declaration(name, clazz, true));
+        contextDeclarations.addDeclarations(new Declaration(name, clazz, true));
         return this;
     }
 
     public Optional<Declaration> findDeclarations(String name) {
-        return declarations.findDeclaration(name);
+        return contextDeclarations.findDeclaration(name);
     }
 
-    public Collection<Declaration> getCreatedDeclarsations() {
-        return declarations.getCreatedDeclarsations();
-    }
-
-    public void addCompilationError(String message) {
-
+    public Optional<Class<?>> findEnum(String name) {
+        try {
+            return Optional.of(typeResolver.resolveType(name));
+        } catch (ClassNotFoundException e) {
+            return Optional.empty();
+        }
     }
 
     public Class<?> resolveType(String name) {
